2bb289aa396b80da21d7432fde391b982b2c3892
Megan Ellman <meganlellman@gmail.com>
2023-01-03 09:55:00 -0800

    updated clear score function

--- assets/script.js
+++ assets/script.js
@@ -20,14 +20,14 @@ var answerKey = [];
 quizStart.addEventListener("click", roundOne);
 
 // Countdown timer
-function countDown(){
-    var timer = setInterval(function(){
+function countDown() {
+    var timer = setInterval(function () {
         document.querySelector("#timer").innerHTML = time;
         time--;
-        if(time < 0){
+        if (time < 0) {
             clearInterval(timer);
             calculateScore()
-        } else if(roundCounter === 5){
+        } else if (roundCounter === 5) {
             clearInterval(timer);
             document.querySelector("#timer").innerHTML = 0;
         }
@@ -36,8 +36,8 @@ function countDown(){
 
 var quesOne = ["1. function myFunction()", "2. myFunction", "3. myFunction()", "4. function myFunction"]
 answerKey.push(quesOne[0]);
-function roundOne(){
-    for(var i = 0; i < btns.length; i++){
+function roundOne() {
+    for (var i = 0; i < btns.length; i++) {
         btns[i].style.visibility = "visible";
         btns[i].textContent = quesOne[i];
     }
@@ -52,8 +52,8 @@ function roundOne(){
 
 var quesTwo = ["1. <link></link>", "2. <a/>", "3. <script/>", "4. <script></script>"]
 answerKey.push(quesTwo[3]);
-function roundTwo(){
-    for(var i = 0; i < btns.length; i++){
+function roundTwo() {
+    for (var i = 0; i < btns.length; i++) {
         btns[i].textContent = quesTwo[i];
     }
     quesNum.textContent = "Question #2";
@@ -63,8 +63,8 @@ function roundTwo(){
 
 var quesThree = ["1. If a is selected, log 'Hello World' to the console", "2. If a is passed, run this function", "3. If a is true, log 'Hello World' to the console", "4. If a is found in the HTML file, log 'Hello World' to the console"]
 answerKey.push(quesThree[2]);
-function roundThree(){
-    for(var i = 0; i < btns.length; i++){
+function roundThree() {
+    for (var i = 0; i < btns.length; i++) {
         btns[i].textContent = quesThree[i];
     }
     quesNum.textContent = "Question #3";
@@ -74,8 +74,8 @@ function roundThree(){
 
 var quesFour = ["1. == is truthy and === is not", "2. == asigns a value and === declares that two values are equal", "3. == declares that two values are equal and === declares that two values are the same primitive type and equal value", "4. === declares that two values are equal and == declares that two values are the same primitive type and equal value"]
 answerKey.push(quesFour[2]);
-function roundFour(){
-    for(var i = 0; i < btns.length; i++){
+function roundFour() {
+    for (var i = 0; i < btns.length; i++) {
         btns.textContent = quesFour[i];
     }
     quesNum.textContent = "Question #4";
@@ -85,8 +85,8 @@ function roundFour(){
 
 // If the content of the button clicked has text content that matches one of these array
 // items, run rightAnswer function. If not, run wrongAnswer function
-function checkAnswer(e){
-    if(answerKey.includes(btns[e].textContent)){
+function checkAnswer(e) {
+    if (answerKey.includes(btns[e].textContent)) {
         rightAnswer();
     } else {
         wrongAnswer();
@@ -94,20 +94,20 @@ function checkAnswer(e){
 }
 
 // Next Round 
-function nextRound(){
-    if(roundCounter === 1){
+function nextRound() {
+    if (roundCounter === 1) {
         roundTwo();
-    } else if(roundCounter === 2){
+    } else if (roundCounter === 2) {
         roundThree();
-    } else if(roundCounter === 3){
+    } else if (roundCounter === 3) {
         roundFour();
-    } else if(roundCounter === 4){
+    } else if (roundCounter === 4) {
         calculateScore();
     }
 }
 
 //Correct
-function rightAnswer(){
+function rightAnswer() {
     answerMessage.style.display = "block";
     correctMessage.style.display = "block";
     setTimeout(cancelMessage, 1000);
@@ -116,7 +116,7 @@ function rightAnswer(){
 }
 
 //Wrong
-function wrongAnswer(){
+function wrongAnswer() {
     answerMessage.style.display = "block";
     wrongMessage.style.display = "block";
     setTimeout(cancelMessage, 1000);
@@ -124,7 +124,7 @@ function wrongAnswer(){
     time--;
 }
 
-function cancelMessage(){
+function cancelMessage() {
     answerMessage.style.display = "none";
     wrongMessage.style.display = "none";
     correctMessage.style.display = "none";
@@ -132,7 +132,7 @@ function cancelMessage(){
 
 // Final Score
 var scoreTotal;
-function calculateScore(){
+function calculateScore() {
     roundCounter++;
     finalScoreMessage.style.display = "block";
     scoreTotal = (finalScore / 4) * 100;
@@ -143,14 +143,14 @@ var scoreContainer = document.getElementById("score-container");
 function getHighScore(form) {
     finalScoreMessage.style.display = "none";
     var entry = document.createElement("p");
-    var name = `${scoreTotal}    -   ${form.initials.value}` ;
+    var name = `${scoreTotal}    -   ${form.initials.value}`;
     entry.appendChild(document.createTextNode(name));
     scoreContainer.appendChild(entry);
     scoreInfo.style.display = "block";
     form.reset();
 }
 
-function playAgain(){
+function playAgain() {
     scoreInfo.style.display = "none";
     time = 30;
     roundCounter = 0;
@@ -158,8 +158,8 @@ function playAgain(){
     roundOne();
 }
 
-function clearScore(){
+function clearScore() {
     while (scoreContainer.hasChildNodes()) {
         scoreContainer.removeChild(scoreContainer.firstChild);
-      }
+    }
 }
